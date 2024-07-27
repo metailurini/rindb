b214f86630c241fe027bbfddf46f2c8a4a21cc83
Shánenoi <shanenoi.org@gmail.com>
2024-07-27 11:18:45 +0700

    Add sync IO

---
+++ io.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+var byteOrder = binary.LittleEndian
+
+func write(storage io.Writer, key, value []byte) error {
+	keyLenBytes := [8]byte{}
+	valueLenBytes := [8]byte{}
+
+	byteOrder.PutUint64(keyLenBytes[:], uint64(len(key)))
+	byteOrder.PutUint64(valueLenBytes[:], uint64(len(value)))
+
+	if _, err := storage.Write(keyLenBytes[:]); err != nil {
+		return fmt.Errorf("failed to write key length: %w", err)
+	}
+
+	if _, err := storage.Write(valueLenBytes[:]); err != nil {
+		return fmt.Errorf("failed to write value length: %w", err)
+	}
+
+	if err := binary.Write(storage, byteOrder, key); err != nil {
+		return fmt.Errorf("failed to write key: %w", err)
+	}
+
+	if err := binary.Write(storage, byteOrder, value); err != nil {
+		return fmt.Errorf("failed to write value: %w", err)
+	}
+
+	return nil
+}
+
+func read(storage io.Reader) (key, value []byte, err error) {
+	keyLenBytes := [8]byte{}
+	if _, err := storage.Read(keyLenBytes[:]); err != nil {
+		return nil, nil, fmt.Errorf("failed to read key length: %w", err)
+	}
+
+	valueLenBytes := [8]byte{}
+	if _, err := storage.Read(valueLenBytes[:]); err != nil {
+		return nil, nil, fmt.Errorf("failed to read value length: %w", err)
+	}
+
+	keyLen := byteOrder.Uint64(keyLenBytes[:])
+	valueLen := byteOrder.Uint64(valueLenBytes[:])
+
+	keyBytes := bytes.NewBuffer([]byte{})
+
+	const defaultReadStep = uint64(255)
+
+	for keyLen > 0 {
+		step := defaultReadStep
+		if keyLen < step {
+			step = keyLen
+		}
+		keyLen -= step
+
+		tempBytes := make([]byte, step)
+
+		if err := binary.Read(storage, byteOrder, tempBytes); err != nil {
+			return nil, nil, fmt.Errorf("failed to read key: %w", err)
+		}
+
+		if _, err := keyBytes.Write(tempBytes); err != nil {
+			return nil, nil, fmt.Errorf("failed to write key: %w", err)
+		}
+	}
+
+	valueBytes := bytes.NewBuffer([]byte{})
+	for valueLen > 0 {
+		step := defaultReadStep
+		if valueLen < step {
+			step = valueLen
+		}
+		valueLen -= step
+
+		tempBytes := make([]byte, step)
+
+		if err := binary.Read(storage, byteOrder, tempBytes); err != nil {
+			return nil, nil, fmt.Errorf("failed to read value: %w", err)
+		}
+
+		if _, err := valueBytes.Write(tempBytes); err != nil {
+			return nil, nil, fmt.Errorf("failed to write value: %w", err)
+		}
+	}
+
+	key = keyBytes.Bytes()
+	value = valueBytes.Bytes()
+
+	return
+}
---
+++ io_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_rw(t *testing.T) {
+	t.Parallel()
+
+	t.Run("write key and value with size > 255", func(t *testing.T) {
+		buf := bytes.NewBufferString("")
+
+		testKey := ""
+		testValue := ""
+		for i := 0; i < 500; i++ {
+			testKey += fmt.Sprintf("test key %d ", i)
+			testValue += fmt.Sprintf("test value %d ", i)
+		}
+
+		err := write(buf, []byte(testKey), []byte(testValue))
+		assert.NoError(t, err)
+
+		key, value, err := read(buf)
+		assert.NoError(t, err)
+		assert.Equal(t, testKey, string(key))
+		assert.Equal(t, testValue, string(value))
+	})
+
+	t.Run("write key and value with size < 255", func(t *testing.T) {
+		buf := bytes.NewBufferString("")
+
+		err := write(buf, []byte("key"), []byte("value"))
+		assert.NoError(t, err)
+
+		key, value, err := read(buf)
+		assert.NoError(t, err)
+		assert.Equal(t, "key", string(key))
+		assert.Equal(t, "value", string(value))
+	})
+}
