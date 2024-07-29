0a364309a2c9b9605baa6978c3c92b4aa9b9f8f2
Shánenoi <shanenoi.org@gmail.com>
2024-07-29 11:19:24 +0700

    Add interface constraint for comparation

---
+++ comparable.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"cmp"
+	"errors"
+)
+
+const UnsupportedTypeCode = -2
+
+var ErrUnsupportedType = errors.New("unsupported type: type does not implement CmpType interface")
+
+type CmpType interface {
+	Compare(other any) int
+}
+
+type Comparable interface {
+	cmp.Ordered | *CmpType | any
+}
+
+func Compare[T Comparable](a, b T) int {
+	switch a := any(a).(type) {
+	case int:
+		return cmp.Compare(a, any(b).(int))
+	case int8:
+		return cmp.Compare(a, any(b).(int8))
+	case int16:
+		return cmp.Compare(a, any(b).(int16))
+	case int32:
+		return cmp.Compare(a, any(b).(int32))
+	case int64:
+		return cmp.Compare(a, any(b).(int64))
+	case uint:
+		return cmp.Compare(a, any(b).(uint))
+	case uint8:
+		return cmp.Compare(a, any(b).(uint8))
+	case uint16:
+		return cmp.Compare(a, any(b).(uint16))
+	case uint32:
+		return cmp.Compare(a, any(b).(uint32))
+	case uint64:
+		return cmp.Compare(a, any(b).(uint64))
+	case uintptr:
+		return cmp.Compare(a, any(b).(uintptr))
+	case float32:
+		return cmp.Compare(a, any(b).(float32))
+	case float64:
+		return cmp.Compare(a, any(b).(float64))
+	case string:
+		return cmp.Compare(a, any(b).(string))
+	case CmpType:
+		return a.Compare(b)
+	default:
+		return UnsupportedTypeCode
+	}
+}
+
+func ValidateCmpType[T Comparable](a T) error {
+	if Compare(a, a) == UnsupportedTypeCode {
+		return ErrUnsupportedType
+	}
+	return nil
+}
---
+++ comparable_test.go
@@ -0,0 +1,359 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockCmpType struct {
+	value int
+}
+
+func (m mockCmpType) Compare(other any) int {
+	o := other.(mockCmpType)
+	if m.value < o.value {
+		return -1
+	} else if m.value > o.value {
+		return 1
+	}
+	return 0
+}
+
+//nolint:gocognit,funlen,gocyclo
+func TestComparable(t *testing.T) {
+	t.Run("int", func(t *testing.T) {
+		tests := []struct {
+			a, b     int
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("int8", func(t *testing.T) {
+		tests := []struct {
+			a, b     int8
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("int16", func(t *testing.T) {
+		tests := []struct {
+			a, b     int16
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("int32", func(t *testing.T) {
+		tests := []struct {
+			a, b     int32
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		tests := []struct {
+			a, b     int64
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("uint", func(t *testing.T) {
+		tests := []struct {
+			a, b     uint
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("uint8", func(t *testing.T) {
+		tests := []struct {
+			a, b     uint8
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("uint16", func(t *testing.T) {
+		tests := []struct {
+			a, b     uint16
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("uint32", func(t *testing.T) {
+		tests := []struct {
+			a, b     uint32
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		tests := []struct {
+			a, b     uint64
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("uintptr", func(t *testing.T) {
+		tests := []struct {
+			a, b     uintptr
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%d vs %d", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%d, %d) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("float32", func(t *testing.T) {
+		tests := []struct {
+			a, b     float32
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%f vs %f", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%f, %f) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("float64", func(t *testing.T) {
+		tests := []struct {
+			a, b     float64
+			expected int
+		}{
+			{1, 2, -1},
+			{2, 1, 1},
+			{1, 1, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%f vs %f", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%f, %f) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("string", func(t *testing.T) {
+		tests := []struct {
+			a, b     string
+			expected int
+		}{
+			{"1", "2", -1},
+			{"2", "1", 1},
+			{"1", "1", 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%s vs %s", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%s, %s) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("MyCmpType", func(t *testing.T) {
+		tests := []struct {
+			a, b     mockCmpType
+			expected int
+		}{
+			{mockCmpType{1}, mockCmpType{2}, -1},
+			{mockCmpType{2}, mockCmpType{1}, 1},
+			{mockCmpType{1}, mockCmpType{1}, 0},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%v vs %v", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.NoError(t, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%v, %v) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+
+	t.Run("Unsupported Type", func(t *testing.T) {
+		tests := []struct {
+			a, b     struct{ int }
+			expected int
+		}{
+			{struct{ int }{1}, struct{ int }{1}, UnsupportedTypeCode},
+		}
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%v vs %v", tt.a, tt.b), func(t *testing.T) {
+				err := ValidateCmpType(tt.a)
+				assert.ErrorIs(t, ErrUnsupportedType, err)
+				result := Compare(tt.a, tt.b)
+				if result != tt.expected {
+					t.Errorf("Compare(%v, %v) = %d; want %d", tt.a, tt.b, result, tt.expected)
+				}
+			})
+		}
+	})
+}
