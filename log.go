31cb8b14c9df0e5004cdce8179990423e2124e32
Shánenoi <shanenoi.org@gmail.com>
2025-04-02 18:37:58 +0700

    Conditionally enable DEBUG logging only during tests

--- log.go
+++ log.go
@@ -1,12 +1,24 @@
 package rindb
 
-import "log"
+import (
+	"flag"
+	"log"
+)
+
+// isRunningTests checks if the code is being run under 'go test'.
+func isRunningTests() bool {
+	return flag.Lookup("test.v") != nil
+}
 
 func LOG(logType string, msg string, args []any) {
 	log.Printf("["+logType+"] "+msg+"\n", args...)
 }
 
-func DEBUG(msg string, args ...any) { LOG("DEBUG", msg, args) }
+func DEBUG(msg string, args ...any) {
+	if isRunningTests() {
+		LOG("DEBUG", msg, args)
+	}
+}
 func INFO(msg string, args ...any)  { LOG("INFO", msg, args) }
 func WARN(msg string, args ...any)  { LOG("WARN", msg, args) }
 func ERROR(msg string, args ...any) { LOG("ERROR", msg, args) }
