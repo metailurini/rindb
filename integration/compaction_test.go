83db3df3449cb3da9fe0e5c6111a0c31b7c5d43e
Shane Nguyễn <shanenoi.org@gmail.com>
2025-08-24 09:08:11 +0700

    test: cover key updates in compaction test

--- integration/compaction_test.go
+++ integration/compaction_test.go
@@ -23,8 +23,8 @@ func TestCompactionMovesSSTablesToNextLevel(t *testing.T) {
 	kvs := []struct{ key, val string }{
 		{"k1", "v1"},
 		{"k2", "v2"},
+		{"k1", "v1_new"}, // update k1 to create multiple versions
 		{"k3", "v3"},
-		{"k4", "v4"},
 	}
 	for _, kv := range kvs {
 		require.NoError(t, db.Put(ctx, rindb.Bytes(kv.key), rindb.Bytes(kv.val)))
@@ -38,9 +38,14 @@ func TestCompactionMovesSSTablesToNextLevel(t *testing.T) {
 		return st.SSTablesPerLevel[0] == 0 && st.SSTablesPerLevel[1] > 0
 	}, 5*time.Second, 100*time.Millisecond, "compaction did not move SSTables to next level")
 
-	for _, kv := range kvs {
-		got, err := db.Get(ctx, rindb.Bytes(kv.key))
+	expectedKVs := map[string]string{
+		"k1": "v1_new",
+		"k2": "v2",
+		"k3": "v3",
+	}
+	for key, val := range expectedKVs {
+		got, err := db.Get(ctx, rindb.Bytes(key))
 		require.NoError(t, err)
-		require.Equal(t, rindb.Bytes(kv.val), got)
+		require.Equal(t, rindb.Bytes(val), got, "key: %s", key)
 	}
 }
