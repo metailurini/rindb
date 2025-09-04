535c4f71309a6c0b07a16a2171be94499c4f185d
Shane Nguyễn <shanenoi.org@gmail.com>
2025-09-04 09:05:14 +0700

    chore: Adjust lock ordering in background compaction
    Reverts the order of mutex locking and manifest rotation in the background compaction goroutine to prevent potential deadlocks.

--- rindb.go
+++ rindb.go
@@ -373,13 +373,14 @@ func (r *Rindb) Put(ctx context.Context, key, value Bytes) error {
 			go func(ctx context.Context, flushSeq uint64) {
 				defer r.wg.Done()
 				INFO(ctx, "Background compaction goroutine started.")
-				r.mu.Lock()
-				defer r.mu.Unlock()
 				if err := r.ssTableManager.Compact(ctx); err != nil {
 					ERROR(ctx, "Background compaction failed: %v", err)
 				} else {
 					INFO(ctx, "Background compaction goroutine finished.")
 				}
+
+				r.mu.Lock()
+				defer r.mu.Unlock()
 
 				if err := r.maybeRotateManifest(ctx); err != nil {
 					ERROR(ctx, "Manifest rotation failed: %v", err)
