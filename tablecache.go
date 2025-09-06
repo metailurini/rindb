2cbaab49db4149f41d41bdeeb618f2bf8f763cfa
Shane Nguyễn <shanenoi.org@gmail.com>
2025-09-06 23:48:04 +0700

    chore: Remove TODO from FDLimiter interface Removes the TODO comment indicating that concrete implementations of FDLimiter should be provided.

--- tablecache.go
+++ tablecache.go
@@ -57,7 +57,6 @@ type tableCacheOptions struct {
 }
 
 // FDLimiter is a minimal semaphore interface (Acquire before Open, Release after Close).
-// TODO: provide concrete implementations.
 type FDLimiter interface {
 	Acquire(ctx context.Context) error
 	Release()
