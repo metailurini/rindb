1f6c801b868da578bc656a63adc4e58d60d0fbf8
Shane Nguyễn <shanenoi.org@gmail.com>
2025-09-10 08:15:04 +0700

    fix: clarify index alignment check

--- sstable.go
+++ sstable.go
@@ -237,7 +237,7 @@ func NewSSTable(ctx context.Context, config Config, fs *FileSystem) (SStable, er
 		errorf(ctx, "index block out of bounds in %s: offset=%d size=%d tail=%d", fs.Path(), f.indexOffset, f.indexSize, tailOffset)
 		return SStable{}, ErrMalFormedSSTable
 	}
-	if f.indexOffset+f.indexSize != uint64(tailOffset) {
+	if f.indexOffset+f.indexSize < uint64(tailOffset) {
 		errorf(ctx, "index block does not align with footer in %s: offset=%d size=%d tail=%d", fs.Path(), f.indexOffset, f.indexSize, tailOffset)
 		return SStable{}, ErrMalFormedSSTable
 	}
