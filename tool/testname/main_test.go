d981556cfaab56aa76385268555854a73c5c61f8
Shane Nguyễn <shanenoi.org@gmail.com>
2025-09-14 15:04:27 +0700

    test: refine testname tool and coverage

--- tool/testname/main_test.go
+++ tool/testname/main_test.go
@@ -19,7 +19,7 @@ func Testbad(t *testing.T) {}
 	require.NoError(t, os.WriteFile(file, []byte(src), 0o644))
 	names, err := checkDir(dir)
 	require.NoError(t, err)
-	require.Equal(t, []string{"Testbad"}, names)
+	require.Equal(t, []string{file + ":4: Testbad"}, names)
 }
 
 func TestCheckDir_AllGood(t *testing.T) {
@@ -29,6 +29,19 @@ import "testing"
 func TestAlpha_Beta(t *testing.T) {}
 `
 	file := filepath.Join(dir, "a_test.go")
+	require.NoError(t, os.WriteFile(file, []byte(src), 0o644))
+	names, err := checkDir(dir)
+	require.NoError(t, err)
+	require.Len(t, names, 0)
+}
+
+func TestCheckDir_IgnoresNonTestFiles(t *testing.T) {
+	dir := t.TempDir()
+	src := `package a
+import "testing"
+func Testbad(t *testing.T) {}
+`
+	file := filepath.Join(dir, "a.go")
 	require.NoError(t, os.WriteFile(file, []byte(src), 0o644))
 	names, err := checkDir(dir)
 	require.NoError(t, err)
