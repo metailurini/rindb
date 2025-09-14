7ac0f81aadaaaafd7acbf36cb9135ba22528d6d3
Shane Nguyễn <shanenoi.org@gmail.com>
2025-09-14 10:23:15 +0700

    test: align unit tests with naming convention

--- fd_limiter_test.go
+++ fd_limiter_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSemaphoreFDLimiterConcurrentGet(t *testing.T) {
+func TestSemaphoreFDLimiter_ConcurrentGet(t *testing.T) {
 	ctx := context.Background()
 	limiter := NewSemaphoreFDLimiter(1)
 
@@ -48,16 +48,39 @@ func TestSemaphoreFDLimiterConcurrentGet(t *testing.T) {
 	require.EqualValues(t, 1, max.Load(), "concurrent opens should be limited to 1")
 }
 
-func TestSemaphoreFDLimiterAcquireCancelled(t *testing.T) {
-	limiter := NewSemaphoreFDLimiter(1)
+func TestSemaphoreFDLimiter_Acquire(t *testing.T) {
+	tests := []struct {
+		name    string
+		ctx     func() context.Context
+		wantErr error
+	}{
+		{
+			name:    "success",
+			ctx:     func() context.Context { return context.Background() },
+			wantErr: nil,
+		},
+		{
+			name: "canceled",
+			ctx: func() context.Context {
+				ctx, cancel := context.WithCancel(context.Background())
+				cancel()
+				return ctx
+			},
+			wantErr: context.Canceled,
+		},
+	}
 
-	require.NoError(t, limiter.Acquire(context.Background()))
-
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-
-	err := limiter.Acquire(ctx)
-	require.ErrorIs(t, err, context.Canceled)
-
-	limiter.Release()
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			limiter := NewSemaphoreFDLimiter(1)
+			err := limiter.Acquire(tt.ctx())
+			if tt.wantErr != nil {
+				require.ErrorIs(t, err, tt.wantErr)
+			} else {
+				require.NoError(t, err)
+				limiter.Release()
+			}
+		})
+	}
 }
