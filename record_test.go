8804ee983245f28e57986bac911c7e371588b95d
Shane Nguyễn <shanenoi.org@gmail.com>
2025-09-14 08:49:37 +0700

    test: refactor unit test names and structure

--- record_test.go
+++ record_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCalOnDiskSize(t *testing.T) {
+func TestCalOnDiskSize_ReturnsExpectedSize(t *testing.T) {
 	type args struct {
 		r Record
 	}
